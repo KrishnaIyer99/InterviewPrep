85320e5f693b15242b1c5ef6db02cf11ff4c23ed
KrishnaIyer99 <krishnaiyer28@gmail.com>
2019-12-03 18:23:04 -0500

    added delete function

--- BST.java
+++ BST.java
@@ -52,30 +52,30 @@ public class BST{
 
 		}
 	}
-	int maxValue(){//get max value in tree
-		Node temp = this.root;
-		int val = 0;
+	int maxValue(Node n){//get max value in tree
+		Node temp = n;
+		int val = n.key;
 		while(temp.right != null){//continue to traverse through right childs
 			val = temp.right.key;
 			temp = temp.right;
 		}
 		return val;
 	}
-	int minValue(){//get min value in tree
-		Node temp = this.root;
-		int val = 0;
+	int minValue(Node n){//get min value in tree
+		Node temp = n;
+		int val = n.key;
 		while(temp.left != null){//continue to traverse through left childs
 			val = temp.left.key;
 			temp = temp.left;
 		}
 		return val;
 	}
-	Node search(int key){//value of node as input, returns null if node not found
-		Node temp = this.root;
+	Node search(int key, Node n){//value of node as input, returns null if node not found
+		Node temp = n;
 		while(temp != null){//keep searching until leaf node is reached
-			System.out.println(temp.key);
+			//System.out.println(temp.key);
 			if(temp.key == key){//check if node has been found
-				System.out.println("Node found!");
+				//System.out.println("Node found!");
 				return temp;
 			}
 			else if(temp.key < key){//go to right child
@@ -85,8 +85,35 @@ public class BST{
 				temp = temp.left;//go to left child
 			}
 		}
-		System.out.println("Node not found");
+		//System.out.println("Node not found");
 		return null;		
+	}
+	Node deleteNode(int key, Node root){//removes specified node and returns updated tree (recursive)
+		//base case (empty)
+		if(root == null){
+			return root;
+		}
+		Node temp = root;//traverse through tree recursively until node is found
+		if(key < temp.key){
+			temp.left = deleteNode(key, temp.left);//left subtree
+		}
+		else if(key > temp.key){
+			temp.right = deleteNode(key, temp.right);//right subtree
+		}
+		else{//Node has been found
+			//check if children exist
+			if(temp.left == null){
+				return temp.right;
+			}
+			else if(temp.right == null){
+				return temp.left;
+			}
+			else{//node with 2 children
+				temp.key = minValue(temp.right);
+				temp.right = deleteNode(temp.key, temp.right);
+			}
+		}
+		return temp;		
 	}
 
 	public static void main(String[] args){//driver program
@@ -98,9 +125,11 @@ public class BST{
 		tree.root = tree.insertNode(tree.root, 70);
 		tree.root = tree.insertNode(tree.root, 60);
 		tree.root = tree.insertNode(tree.root, 80);
-		System.out.println(tree.minValue());
-		//tree.inOrder(tree.root);
-		tree.search(60);
-		tree.search(25);
+		tree.root = tree.deleteNode(20, tree.root);
+		tree.root = tree.deleteNode(30, tree.root);
+		//System.out.println(tree.minValue(tree.root));
+		tree.inOrder(tree.root);
+		//tree.search(60);
+		//tree.search(25);
 	}
 }

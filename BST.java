3bb67eb0d139962f664699c74582e51fb7974d45
KrishnaIyer99 <krishnaiyer28@gmail.com>
2019-12-03 03:59:30 -0500

    min, max, search functions - added comments

--- BST.java
+++ BST.java
@@ -1,29 +1,31 @@
 public class BST{
-	class Node{
+	class Node{//sub-class for node objects in BST
         	int key;
-        	Node left, right;
-        	public Node(int data){
+        	Node left, right; //left and right child
+        	public Node(int data){//Node object constructor
        	        	key = data;
                 	left = right = null;
         	}
 	}
 
-	Node root;
+	Node root;//head of tree
+
+	//constructors for BST object
 	public BST(int data){
 		root = new Node(data);
 	}
 	public BST(){
 		root = null;
 	}
-	Node insertNode(Node root, int data){
-		if(root == null){
+	Node insertNode(Node root, int data){//insert function returns root of tree
+		if(root == null){//base case (empty tree)
 			root = new Node(data);
 			return root;
 		}
-		Node temp = root;
+		Node temp = root;//create temporary object to traverse through tree
 		while(true){
-			if(temp.key < data){
-				if(temp.right == null){
+			if(temp.key < data){//traverse right sub-tree
+				if(temp.right == null){// if right child does not exist
 					temp.right = new Node(data);
 					return root;
 				}
@@ -31,8 +33,8 @@ public class BST{
 					temp = temp.right;
 				}
 			}
-			else{
-				if(temp.left == null){
+			else{//traverse left subtree
+				if(temp.left == null){//if left child does not exist
 					temp.left = new Node(data);
 					return root;
 				}
@@ -42,15 +44,52 @@ public class BST{
 			}
 		}
 	}
-	void inOrder(Node root){
+	void inOrder(Node root){//inorder traversal of tree (recursively)
 		if(root != null){
 			inOrder(root.left);
-	                System.out.println(root.key);
-        	        inOrder(root.right);
+			System.out.println(root.key);
+			inOrder(root.right);
 
 		}
 	}
-	public static void main(String[] args){
+	int maxValue(){//get max value in tree
+		Node temp = this.root;
+		int val = 0;
+		while(temp.right != null){//continue to traverse through right childs
+			val = temp.right.key;
+			temp = temp.right;
+		}
+		return val;
+	}
+	int minValue(){//get min value in tree
+		Node temp = this.root;
+		int val = 0;
+		while(temp.left != null){//continue to traverse through left childs
+			val = temp.left.key;
+			temp = temp.left;
+		}
+		return val;
+	}
+	Node search(int key){//value of node as input, returns null if node not found
+		Node temp = this.root;
+		while(temp != null){//keep searching until leaf node is reached
+			System.out.println(temp.key);
+			if(temp.key == key){//check if node has been found
+				System.out.println("Node found!");
+				return temp;
+			}
+			else if(temp.key < key){//go to right child
+				temp = temp.right;
+			}
+			else{
+				temp = temp.left;//go to left child
+			}
+		}
+		System.out.println("Node not found");
+		return null;		
+	}
+
+	public static void main(String[] args){//driver program
 		BST tree = new BST();
 		tree.root = tree.insertNode(tree.root, 50);
 		tree.root = tree.insertNode(tree.root, 30);
@@ -59,7 +98,9 @@ public class BST{
 		tree.root = tree.insertNode(tree.root, 70);
 		tree.root = tree.insertNode(tree.root, 60);
 		tree.root = tree.insertNode(tree.root, 80);
-		//System.out.println(tree.root.left.key);
-		tree.inOrder(tree.root);
+		System.out.println(tree.minValue());
+		//tree.inOrder(tree.root);
+		tree.search(60);
+		tree.search(25);
 	}
 }
